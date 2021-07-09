3cd26c3e1eee29a456d64310d3870adbf3eff637
Joseph Metzinger <joseph.metzinger@gmail.com>
2021-07-09 10:03:57 -0700

    Revert "name change"
    This reverts commit 1645f08c6954f52192c7bf22ced220d532e4ea51.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ version = re.search(
 ).group(1)
 
 
-NAME = 'stacksi-python-docx'
+NAME = 'python-docx'
 VERSION = version
 DESCRIPTION = 'Create and update Microsoft Word .docx files.'
 KEYWORDS = 'docx office openxml word'
